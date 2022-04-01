5cc16817e4e9638b1d274a7f185c60c3a15fbc45
michael09232002 <102804007+michael09232002@users.noreply.github.com>
2022-04-01 05:15:48 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,30 @@
     </head>
     <body>
     <h1>Welcome to My Website! I am Michael John Diopenes! </h1>
-    <ul>
-        <li><a href="index.php">HOME</a></li>
-    </ul>
+    <div class="nav">
+        <ul>
+            <li><a href="#">Home<i class="fa fa-home"></i></a></li>
+            <li><a href="#">About us<i class="fa fa-user"></i></a>
+            <div class="sub1">
+                <ul>
+                    <li><a href="#">Map</a></li>
+                    <li><a href="#>Leaders</a></li>
+                    <li><a href="#">History</a></li>
+                    <li><a href="#">Culture</a></li>
+                </ul>
+            </div>
+        </li>
+            <li><a href="#">Foods<i class="fa fa-cutlery"></i></a>
+            <div class="sub2">
+                <ul>
+                    <li><a href="#">Street Foods</a></li>
+                    <li><a href="#">5 Star Dinning</a></li>
+                    <li><a href="#>Exotic</a></li>
+                </ul>
+            </div>
+        </li>
+            <li><a href="#">Tourist spots<i class="fa fa-map-marker"></i></a></li>
+        </ul>
+    </div>
     </body>
    </html>
