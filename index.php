bce9c1242c3dfc14ded4265d3fdd5049367bbdff
michael09232002 <102804007+michael09232002@users.noreply.github.com>
2022-04-01 04:05:55 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Michael Gwapo</title>
+    </head>
+    <body>
+    <h1>Welcome to My Website! I am Michael John Diopenes! </h1>
+    <ul>
+        <li><a href="index.php">HOME</a></li>
+    </ul>
+    </body>
+   </html>
